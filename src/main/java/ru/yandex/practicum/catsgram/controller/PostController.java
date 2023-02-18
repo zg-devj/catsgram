c3475fc396305c45ce6b0a7c36047a6aafcd284d
Gleb Zakharov <webmaster-80@yandex.ru>
2023-02-18 22:01:40 +0300

    feat: добавить метод findById для Post

--- src/main/java/ru/yandex/practicum/catsgram/controller/PostController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/PostController.java
@@ -1,14 +1,12 @@
 package ru.yandex.practicum.catsgram.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.catsgram.model.Post;
 import ru.yandex.practicum.catsgram.service.PostService;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Контроллер постов
@@ -24,22 +22,19 @@ public class PostController {
         this.postService = postService;
     }
 
-    /**
-     * Возвращает список постов. GET /posts
-     *
-     * @return Список постов
-     */
+    // Возвращает список постов
     @GetMapping("/posts")
-    private List<Post> findAll() {
+    public List<Post> findAll() {
         return postService.findAll();
     }
 
-    /**
-     * Добавление нового поста к списку постов. POST /post
-     *
-     * @param post новый пост
-     * @return новый пост
-     */
+    // Возвращаем пост по postId
+    @GetMapping("/post/{postId}")
+    public Post findById(@PathVariable int postId) {
+        return postService.findById(postId);
+    }
+
+    // Добавление нового поста к списку постов
     @PostMapping(value = "/post")
     public Post create(@RequestBody Post post) {
         return postService.create(post);
--- src/main/java/ru/yandex/practicum/catsgram/service/PostService.java
+++ src/main/java/ru/yandex/practicum/catsgram/service/PostService.java
@@ -4,15 +4,18 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import ru.yandex.practicum.catsgram.exception.PostNotFoundException;
 import ru.yandex.practicum.catsgram.exception.UserNotFoundException;
 import ru.yandex.practicum.catsgram.model.Post;
 import ru.yandex.practicum.catsgram.model.User;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PostService {
+    private int id = 0;
     //Список постов
     private List<Post> posts = new ArrayList<>();
 
@@ -39,8 +42,23 @@ public class PostService {
                     "Пользователь {} не найден", post.getAuthor());
             throw new UserNotFoundException("Пользователь " + post.getAuthor() + " не найден");
         }
+        post.setId(++id);
         posts.add(post);
         log.debug("Добавлен пост: {}", post);
         return post;
+    }
+
+    // Вернуть пост по postId
+    public Post findById(int postId) {
+        Optional<Post> post = posts.stream()
+                .filter(x -> x.getId() == postId)
+                .findFirst();
+        if (post.isPresent()) {
+            log.debug("Запрошен пост с id={}", postId);
+            return post.get();
+        } else {
+            log.warn("Запрошен не существующий пост с id={}", postId);
+            throw new PostNotFoundException("Пост c №" + postId + " не найден.");
+        }
     }
 }
